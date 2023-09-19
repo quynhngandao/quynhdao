f37d67a4de0cf6b9e5b4599f54e6b260b83739a2
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-18 20:21:08 -0500

    N/A

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import genderstanding from "./genderstanding.png";
 import playwme from "./playwme.png";
 import { Card, CardMedia, CardContent, CardActionArea } from "@mui/material";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Projects() {
   return (
@@ -11,11 +10,11 @@ function Projects() {
         <div className="w-full sm:w-1/2 md:w-full lg:w-full ">
           <Card sx={{borderRadius:4}} className="w-full text-center">
             <h2 className="pt-4 pb-0">
-              <a style={{color:"#36373a"}} href="https://genderstanding-116c2268e46c.herokuapp.com/#/welcome">
+              <a style={{color:"#36373a"}} href="#/projects">
                 Genderstanding
               </a>
             </h2>
-
+{/* https://genderstanding-116c2268e46c.herokuapp.com/#/welcome */}
             <CardMedia
               component="img"
               image={genderstanding}
@@ -37,7 +36,7 @@ function Projects() {
             <ul className="actions special">
               <li>
                 <a style={{borderRadius:10, verticalAlign:"middle", color:"#36373a" }}
-                  href="https://genderstanding-116c2268e46c.herokuapp.com/#/welcome"
+                  href="#/projects"
                   className="button"
                 >
                   See Project
@@ -75,7 +74,7 @@ function Projects() {
             <ul className="actions special">
               <li>
                 <a style={{borderRadius:10,  verticalAlign:"middle" , color:"#36373a"}}
-                  href="https://genderstanding-116c2268e46c.herokuapp.com/#/welcome"
+                  href="https://playwme-1cf74549f561.herokuapp.com/#/home"
                   className="button"
                 >
                   See Project
