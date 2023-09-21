92aab63e8b6bd3f65a06572ffd4670a6f5c78534
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-21 16:23:26 -0500

    fix some styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,10 +6,10 @@ export default function About() {
   return (
     <div id="about" >
     <div className="relative overflow-hidden bg-white ">
-      <div className="mx-auto md:max-w-7xl ">
-        <div className="relative z-10 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <div className="mx-auto  md:max-w-8xl">
+        <div className="relative z-10 bg-white sm:pb-16 md:pb-20 md:max-w-3xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
-            className="absolute inset-y-0 right-0 hidden h-full text-white transform translate-x-1/2 w-52 lg:block"
+            className="absolute inset-y-0 right-0 hidden h-full text-white transform translate-x-1/2 w-60 lg:block"
             fill="currentColor"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
@@ -20,9 +20,9 @@ export default function About() {
 
           <div className="pt-1"></div>
 
-          <main className="items-center justify-center lg:max-w-7xl sm:mt-12 sm:px-6 md:mt-16 md:px-8 lg:mt-20 lg:px-10">
+          <main className="items-center justify-center sm:mt-12 sm:px-6 md:mt-16 md:px-8 lg:mt-20 lg:px-10">
 
-            <div className=" sm:text-center lg:text-center md:text-center">
+            <div className="aboutme 2xl:ml-5 sm:text-center lg:text-center md:text-center">
               <h2 className="my-6 text-2xl font-extrabold tracking-tight text-center text-gray-700 sm:text-3xl md:text-4xl">
               <a
               target="_blank"
@@ -68,8 +68,7 @@ export default function About() {
         </h2>
         <img
           className="object-cover object-bottom w-full sm:h-72 md:h-full lg:w-full lg:h-full"
-          src="https://scontent.ffcm1-2.fna.fbcdn.net/v/t1.6435-9/65309185_2545513058813810_2896765178395754496_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=Qr6aUewnaJsAX8Oo_mz&_nc_ht=scontent.ffcm1-2.fna&oh=00_AfCqZ_QUoCdj-qGiVDwmrta-Z8kWDfrvowRMkyczI6Kd2w&oe=650A6E81"
-          alt=""
+          src="https://camo.githubusercontent.com/7b6d7cf8f8ee80c529bad7438583345cc15e2b4bf376b4272099842ca832fda6/68747470733a2f2f73636f6e74656e742d6d7370312d312e78782e666263646e2e6e65742f762f74312e363433352d392f36353331353630305f323534353531323534353438303532385f313230323933373432353236353935303732305f6e2e6a70673f5f6e635f6361743d313031266363623d312d37265f6e635f7369643d376638633738265f6e635f6f68633d4c775959423841305278634158396834383778265f6e635f68743d73636f6e74656e742d6d7370312d312e7878266f683d30305f41664371676e4f6c63793362384e533758734c6f775a665744435974435f455577597834713656527270756c4b77266f653d3635303231304434"   alt=""
         />
       </div>
     </div>
