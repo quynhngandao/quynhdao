fac722ba318bf2d3f6c3760cf464ad5a70656a00
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-23 21:11:35 -0500

    added animation to hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,19 +1,143 @@
-import React from 'react';
-import '../App.css';
-import './HeroSection.css';
+import "../App.css";
+import "./HeroSection.css";
+import React, { useLayoutEffect, useState } from "react";
+import {
+  useSpringRef,
+  animated,
+  useTransition,
+  useSpring,
+} from "@react-spring/web";
 
 function HeroSection() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [fontSize, setFontSize] = useState(50);
+  const [fontSize2, setFontSize2] = useState(60);
+  const springApi = useSpringRef();
+
+  const name = ["Hello, I'm Quynh", "Full-Stack Developer"];
+
+  const transitions = useTransition(activeIndex, {
+    from: {
+      clipPath: "polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)",
+      fontSize: fontSize, // Use the dynamic font size here
+    },
+    enter: {
+      clipPath: "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)",
+      fontSize: fontSize2, // Use the dynamic font size here
+    },
+    leave: {
+      clipPath: "polygon(100% 0%, 100% 100%, 100% 100%, 100% 0%)",
+      fontSize: fontSize, // Use the dynamic font size here
+    },
+    onRest: (_springs, _ctrl, item) => {
+      if (activeIndex === item) {
+        setActiveIndex(activeIndex === name.length - 1 ? 0 : activeIndex + 1);
+      }
+    },
+    exitBeforeEnter: true,
+    config: {
+      duration: 1500,
+    },
+    delay: 500,
+    ref: springApi,
+  });
+
+  // Update font size based on screen width
+  useLayoutEffect(() => {
+    const fontSizes = {
+      xlargeScreen: 90,
+      largeScreen: 50,
+      mediumScreen: 40,
+      smallScreen: 25,
+      xsmallScreen: 20,
+    };
+  
+    const fontSizes2 = {
+      xlargeScreen: 100,
+      largeScreen: 60,
+      mediumScreen: 50,
+      smallScreen: 30,
+      xsmallScreen: 25,
+    };
+  
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 390) {
+        setFontSize(fontSizes.xsmallScreen);
+        setFontSize2(fontSizes2.xsmallScreen);
+      }
+      else if (windowWidth <= 768) {
+        setFontSize(fontSizes.smallScreen);
+        setFontSize2(fontSizes2.smallScreen);
+      } else if (windowWidth <= 1024) {
+        setFontSize(fontSizes.mediumScreen);
+        setFontSize2(fontSizes2.mediumScreen);
+      } else if (windowWidth <= 1440) {
+        setFontSize(fontSizes.largeScreen);
+        setFontSize2(fontSizes2.largeScreen);
+      } else if (windowWidth <= 2560) {
+        setFontSize(fontSizes.xlargeScreen);
+        setFontSize2(fontSizes2.xlargeScreen);
+      } else {
+        // Set default font sizes here
+        setFontSize(fontSizes.smallScreen); // You can change this to your desired default size
+        setFontSize2(fontSizes2.smallScreen); // You can change this to your desired default size
+      }
+    };
+  
+    // Initial font size update
+    handleResize();
+  
+    window.addEventListener("resize", handleResize);
+  
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  
+
+  const springs = useSpring({
+    from: {
+      strokeDashoffset: 120,
+    },
+    to: {
+      strokeDashoffset: 0,
+    },
+    config: {
+      duration: 5000,
+    },
+    loop: true,
+    ref: springApi,
+  });
+
+  useLayoutEffect(() => {
+    springApi.start();
+  }, [activeIndex]);
 
   return (
-    <div >
-      
-    <div className=' hero-container'>
-      {/* <video src='https://vod-progressive.akamaized.net/exp=1695351645~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4728%2F9%2F248644905%2F900760521.mp4~hmac=5071e70fe2aeff888c304d72547928e9f37fbadc4653d60648ab878e7bb23844/vimeo-prod-skyfire-std-us/01/4728/9/248644905/900760521.mp4?filename=file.mp4' autoPlay loop muted /> */}
-      <h1 className='justify-items intro-title align-center'>Hi, I'm Quynh </h1>
-      <p className='intro-paragraph'> Full-Stack Developer</p>
-      <div className='hero-btns'>
+    <div>
+      <div className="hero-container">
+        <div className="container">
+          <div className="container__inner">
+            {transitions((springs, item) => (
+              <animated.h1
+                className="h1__container"
+                style={{
+                  ...springs,
+                  color: "white",
+                  fontFamily: "fraunces",
+                  fontWeight: 900,
+                }}
+              >
+                {name[item]}
+              </animated.h1>
+            ))}
+            <div className="ticker">
+              <div />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
